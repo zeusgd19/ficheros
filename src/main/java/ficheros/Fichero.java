3d252c61325a6d6ca97052ebfef9638bf2141fbe
zeusgd19 <dariusgd19@gmail.com>
2024-04-29 09:20:38 +0200

    Second Commit

--- src/main/java/ficheros/Fichero.java
+++ src/main/java/ficheros/Fichero.java
@@ -1,12 +1,17 @@
-package sistemaficheros;
+package ficheros;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Fichero {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         File f = File.listRoots()[0];
+        System.out.println("-------------------------------------------");
+        System.out.println("LISTA DE FICHEROS Y DIRECTORIOS DEL SISTEMA");
+        System.out.println("-------------------------------------------");
         listar(f);
         int option = sc.nextInt();
         while (option != -1){
@@ -27,12 +32,13 @@ public class Fichero {
             }
             i++;
         }
+        System.out.println("A que directorio te quiere mover: ");
     }
 
-    public static File move(File f, int mv){
+    public static File move(File f, int mv) {
         if(mv == 0){
             return f.getParentFile();
         }
-        return f.listFiles()[mv -1].isFile() ? f:f.listFiles()[mv - 1];
+        return f.listFiles()[mv - 1].isFile() ? f:f.listFiles()[mv -1];
     }
 }
