d74def1c2e9211937450ec82e36f158c821474b6
zeusgd19 <dariusgd19@gmail.com>
2024-04-29 09:51:48 +0200

    Cosas añadidas

--- src/main/java/ficheros/Fichero.java
+++ src/main/java/ficheros/Fichero.java
@@ -1,8 +1,10 @@
 package ficheros;
 
 import java.io.File;
+import java.text.DateFormat;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Fichero {
@@ -12,23 +14,45 @@ public class Fichero {
         System.out.println("-------------------------------------------");
         System.out.println("LISTA DE FICHEROS Y DIRECTORIOS DEL SISTEMA");
         System.out.println("-------------------------------------------");
-        listar(f);
+        list(f);
         int option = sc.nextInt();
         while (option != -1){
             f = move(f,option);
-            listar(f);
+            list(f);
             option= sc.nextInt();
         }
     }
 
-    public static void listar(File f){
+    public static void list(File f){
         int i = 1;
         System.out.println("0 DIRECTORIO PADRE <DIRECTORIO>");
+        String permisosDir = "d---";
+        String permisosFile = "----";
+        DateFormat formatter;
+        formatter = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM, Locale.getDefault());
         for (File fi : f.listFiles()){
             if(fi.isDirectory()){
-                System.out.println(i + " " + fi.getName() + " <Directoio>");
+                if(fi.canRead() && fi.canWrite() && fi.canExecute()){
+                    permisosDir = "drwx";
+                } else if(fi.canRead()){
+                    permisosDir = "dr--";
+                } else if(fi.canWrite()){
+                    permisosDir = "d-w-";
+                } else if(fi.canExecute()){
+                    permisosDir = "d--x";
+                }
+                System.out.println(i + "\t\t" + permisosDir + "\t\t" +  fi.length() / 1024 + "MB " + "\t\t" + formatter.format(fi.lastModified()) + "\t" +  fi.getName() + " <Directoio>");
             } else {
-                System.out.println(i + " " + fi.getName() + " <Archivo>");
+                if(fi.canRead() && fi.canWrite() && fi.canExecute()){
+                    permisosFile = "-rwx";
+                } else if(fi.canRead()){
+                    permisosFile = "-r--";
+                } else if(fi.canWrite()){
+                    permisosFile = "--w-";
+                } else if(fi.canExecute()){
+                    permisosFile = "---x";
+                }
+                System.out.println(i + "\t\t" + permisosFile + "\t\t" + fi.length() / 1024 + "MB " + "\t\t" + formatter.format(fi.lastModified()) + "\t" +  fi.getName() + " <Archivo>");
             }
             i++;
         }
